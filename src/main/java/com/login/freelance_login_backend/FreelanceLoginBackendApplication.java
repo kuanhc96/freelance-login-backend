a02c3ad22b8fb542c1a57fc6f1988baa5adba928
Kuan <chenkuansk@ad.garmin.com>
2025-02-24 22:43:08 +0800

    initial commit

---
+++ src/main/java/com/login/freelance_login_backend/FreelanceLoginBackendApplication.java
@@ -0,0 +1,13 @@
+package com.login.freelance_login_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceLoginBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceLoginBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/login/freelance_login_backend/FreelanceLoginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.freelance_login_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceLoginBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
